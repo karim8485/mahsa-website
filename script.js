c896fabe4875030305b863a1425153bfb2ce57f8
karim8485 <kareemkidevu@gmail.com>
2025-01-14 20:03:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example: Hide an element after a button click
+document.querySelector('#someButton').addEventListener('click', function() {
+    document.querySelector('#someElement').style.display = 'none';
+});
